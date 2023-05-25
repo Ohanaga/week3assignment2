fa194ce17d93bef51887207865e577777d14539b
Ohanaga <133486437+Ohanaga@users.noreply.github.com>
2023-05-25 10:22:27 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Ohanaga Perpetua Chiebuka";
+$regnumber = "2021/HND/33296/CS";
+$soo = "Imo State";
+$topic_of_project = "Online Student Identity Verification System using Facial Recognition";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic_of_project<br>";
+
+?>
